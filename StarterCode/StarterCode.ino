598c72af7cff7aa7e5ff76ca0cdff0ddd8d29c54
Paul <paul.nadan@students.olin.edu>
2017-11-12 19:59:25 -0500

    Added sumo mode

--- StarterCode/StarterCode.ino
+++ StarterCode/StarterCode.ino
@@ -41,10 +41,40 @@ extern int32_t speedRight;
 extern int32_t distanceLeft;
 extern int32_t distanceRight;
 
+float testSpeed = 0;          // this is the desired motor speed
+float mode = 3; // 0 = balance, 1 = race, 2 = turn, 3 = sumo
+
+static float IL = 0;
+static float IR = 0;
+static float kpV = 500;
+static float alpha  = 17;
+static float beta   = 1.0 / 400.0;
+static float kiV = 5000;//alpha*(1+beta*kpV)*(1+beta*kpV)/(4*beta);
+static float kpT = 6;
+static float g = 9.81;
+static float l = 0.089;
+static float kiT = 36;//g+kpT*kpT/(4*l);
+static float Itheta = 0;
+static float IV = 0;
+static float IVThreshold = 15.0 / 180.0 * PI;
+static float kiEpsilon = 0.2; //0.1
+static float kdEpsilon = 0;
+static float epsilon = 0; //0;
+static float vD = 0;
+
+static float IV0 = 0.1;
+static float vOffset = 0.2; // Changes target position over time, 0.2
+static float sumoOffset = 0.2; // Changes target position over time, 0.2
+static float turnRate = 0.04;
+
+static float prevVL = 0;
+static float prevVR = 0;
+static float maxDeltaV = 4;
+
 void balanceDoDriveTicks();
 
 extern int32_t displacement;
-int32_t prev_displacement=0;
+int32_t prev_displacement = 0;
 
 LSM6 imu;
 Balboa32U4Motors motors;
@@ -65,6 +95,7 @@ void setup()
   angle_accum = 0;
   ledGreen(0);
   ledYellow(0);
+  if (mode==1||mode==3) IV = IV0;
 }
 
 extern int16_t angle_prev;
@@ -80,14 +111,16 @@ void newBalanceUpdate()
   static uint32_t lastMillis;
   uint32_t ms = millis();
 
-  if ((uint32_t)(ms - lastMillis) < UPDATE_TIME_MS) { return; }
+  if ((uint32_t)(ms - lastMillis) < UPDATE_TIME_MS) {
+    return;
+  }
   balanceUpdateDelayedStatus = ms - lastMillis > UPDATE_TIME_MS + 1;
   lastMillis = ms;
 
   // call functions to integrate encoders and gyros
   balanceUpdateSensors();
- 
-   if (imu.a.x < 0)
+
+  if (imu.a.x < 0)
   {
     lyingDown();
     isBalancingStatus = false;
@@ -98,36 +131,8 @@ void newBalanceUpdate()
   }
 }
 
-
-float testSpeed = 0;          // this is the desired motor speed
-
-static float IL = 0;
-static float IR = 0;
-static float kpV = 500;
-static float alpha  = 17;
-static float beta   = 1.0/400.0;
-static float kiV = 5000;//alpha*(1+beta*kpV)*(1+beta*kpV)/(4*beta);
-static float kpT = 6;
-static float g = 9.81;
-static float l = 0.089;
-static float kiT = 36;//g+kpT*kpT/(4*l);
-static float Itheta = 0;
-static float IV = 0;
-static float IVThreshold = 15.0/180.0*PI;
-static float kiEpsilon = 0.2; //0.1
-static float kdEpsilon = 0;
-static float epsilon = 0; //0;
-static float vD = 0;
-
-static float vOffset = .2;
-static float turnRate = 0.04;
-
-static float prevVL = 0;
-static float prevVR = 0;
-static float maxDeltaV = 4;
-
 float clamp(float a, float mi, float ma) {
-  return min(max(a,mi),ma);
+  return min(max(a, mi), ma);
 }
 
 void loop()
@@ -145,49 +150,49 @@ void loop()
 
 
 
-  newBalanceUpdate();                    // run the sensor updates. this function checks if it has been 10 ms since the previous 
-  
-  if(angle > 3000 || angle < -3000)      // If angle is not within +- 3 degrees, reset counter that waits for start
+  newBalanceUpdate();                    // run the sensor updates. this function checks if it has been 10 ms since the previous
+
+  if (angle > 3000 || angle < -3000)     // If angle is not within +- 3 degrees, reset counter that waits for start
   {
     start_counter = 0;
   }
 
   bool shouldPrint = cur_time - prev_print_time > 105;
-  if(shouldPrint)   // do the printing every 105 ms. Don't want to do it for an integer multiple of 10ms to not hog the processor
+  if (shouldPrint)  // do the printing every 105 ms. Don't want to do it for an integer multiple of 10ms to not hog the processor
   {
-        Serial.print(cur_time);   
-        Serial.print("\t");
-        Serial.print(speedLeft);   
-        Serial.print("\t");
-        Serial.print(speedRight);
-        Serial.print("\t");
-        Serial.print(IL);
-        Serial.print("\t");
-        Serial.print(IR);
-        Serial.print("\t");
-        Serial.print(IV);
-        Serial.print("\t");
-        Serial.print(Itheta);
-        Serial.print("\t");
-        Serial.print(vD);
-        Serial.println("");
-        prev_print_time = cur_time;
+    Serial.print(cur_time);
+    Serial.print("\t");
+    Serial.print(speedLeft);
+    Serial.print("\t");
+    Serial.print(speedRight);
+    Serial.print("\t");
+    Serial.print(IL);
+    Serial.print("\t");
+    Serial.print(IR);
+    Serial.print("\t");
+    Serial.print(IV);
+    Serial.print("\t");
+    Serial.print(Itheta);
+    Serial.print("\t");
+    Serial.print(vD);
+    Serial.println("");
+    prev_print_time = cur_time;
   }
 
-  float delta_t = (cur_time - prev_time)/1000.0;
+  float delta_t = (cur_time - prev_time) / 1000.0;
 
   // handle the case where this is the first time through the loop
   if (prev_time == 0) {
     delta_t = 0.01;
   }
-  
+
   // every UPDATE_TIME_MS, check if angle is within +- 3 degrees and we haven't set the start flag yet
-  if(cur_time - prev_time > UPDATE_TIME_MS && angle > -3000 && angle < 3000 && !armed_flag)   
+  if (cur_time - prev_time > UPDATE_TIME_MS && angle > -3000 && angle < 3000 && !armed_flag)
   {
     // increment the start counter
     start_counter++;
     // If the start counter is greater than 30, this means that the angle has been within +- 3 degrees for 0.3 seconds, then set the start_flag
-    if(start_counter > 30)
+    if (start_counter > 30)
     {
       armed_flag = 1;
       buzzer.playFrequency(DIV_BY_10 | 445, 1000, 15);
@@ -195,11 +200,11 @@ void loop()
   }
 
   // angle is in millidegrees, convert it to radians and subtract the desired theta
-  angle_rad = ((float)angle)/1000/180*3.14159 - del_theta;
+  angle_rad = ((float)angle) / 1000 / 180 * 3.14159 - del_theta;
 
   // only start when the angle falls outside of the 3.0 degree band around 0.  This allows you to let go of the
   // robot before it starts balancing
-  if(cur_time - prev_time > UPDATE_TIME_MS && (angle < -3000 || angle > 3000) && armed_flag)   
+  if (cur_time - prev_time > UPDATE_TIME_MS && (angle < -3000 || angle > 3000) && armed_flag)
   {
     start_flag = 1;
     armed_flag = 0;
@@ -207,81 +212,92 @@ void loop()
     IR = 0;
     Itheta = 0;
     IV = 0;
+    if (mode==1||mode==3) IV = IV0;
   }
 
   // every UPDATE_TIME_MS, if the start_flag has been set, do the balancing
-  if(cur_time - prev_time > UPDATE_TIME_MS && start_flag)
+  if (cur_time - prev_time > UPDATE_TIME_MS && start_flag)
   {
     // set the previous time to the current time for the next run through the loop
     prev_time = cur_time;
 
     // speedLeft and speedRight are just the change in the encoder readings
     // wee need to do some math to get them into m/s
-    float vL = METERS_PER_CLICK*speedLeft/delta_t;
-    float vR = METERS_PER_CLICK*speedRight/delta_t;
+    float vL = METERS_PER_CLICK * speedLeft / delta_t;
+    float vR = METERS_PER_CLICK * speedRight / delta_t;
 
-    if(abs(vL-prevVL) > maxDeltaV) {
+    if (abs(vL - prevVL) > maxDeltaV) {
       vL = prevVL;
     }
 
-    if(abs(vR-prevVR) > maxDeltaV) {
+    if (abs(vR - prevVR) > maxDeltaV) {
       vR = prevVR;
     }
 
     prevVL = vL;
     prevVR = vR;
 
-    float v = (vL+vR)/2;
-    
-    
+    float v = (vL + vR) / 2;
+
+
     // set PWM_left and PWM_right here
     float PWM_left;
     float PWM_right;
 
-    
-    float errTheta = angle_rad+IV*kiEpsilon+v*kdEpsilon;
+
+    float errTheta = angle_rad + IV * kiEpsilon + v * kdEpsilon;
     //angle = angle * 999 / 1000;
-    
-    Itheta += errTheta*delta_t;
-    vD = kpT*errTheta + kiT*Itheta;
-    float errL = (vD)-vL;
-    float errR = (vD)-vR;
-    IL += errL*delta_t;
-    IR += errR*delta_t;
-    PWM_left = errL*kpV + IL*kiV;
-    PWM_right = errR*kpV + IR*kiV;
-    IV += (vOffset+v)*delta_t;
+
+    Itheta += errTheta * delta_t;
+    vD = kpT * errTheta + kiT * Itheta;
+    float errL = (vD) - vL;
+    float errR = (vD) - vR;
+    if (mode==2) errL -= turnRate; // Turn mode: offset wheel velocities
+    if (mode==2) errR += turnRate; // Turn mode: offset wheel velocities
+    IL += errL * delta_t;
+    IR += errR * delta_t;
+    PWM_left = errL * kpV + IL * kiV;
+    PWM_right = errR * kpV + IR * kiV;
+    IV += v * delta_t;
+    if (mode==3 && v > 0 && IV < 0) { // Sumo mode: recover from impacts
+      IV = 0.2;
+      IL = 0;
+      IR = 0;
+    }
+    if (mode==3) IV += sumoOffset * delta_t; // Sumo mode: increase target position over time
+    if (mode==1) IV += vOffset * delta_t; // Race mode: increase target position over time
     //IV = IV * 9995 / 10000;
 
-    if(abs(PWM_left) > MOTOR_MAX) {
-      PWM_left = (PWM_left>0?1:-1)*MOTOR_MAX;
+    if (abs(PWM_left) > MOTOR_MAX) {
+      PWM_left = (PWM_left > 0 ? 1 : -1) * MOTOR_MAX;
     }
-    if(abs(PWM_right) > MOTOR_MAX) {
-      PWM_right = (PWM_right>0?1:-1)*MOTOR_MAX;
+    if (abs(PWM_right) > MOTOR_MAX) {
+      PWM_right = (PWM_right > 0 ? 1 : -1) * MOTOR_MAX;
     }
-    
-    
+
+
 
     // if the robot is more than 45 degrees, shut down the motor
-    if(start_flag && fabs(angle_rad) > FORTY_FIVE_DEGREES_IN_RADIANS) // TODO: this was set to angle < -0.78... I changd it to angle_rad
+    if (start_flag && fabs(angle_rad) > FORTY_FIVE_DEGREES_IN_RADIANS) // TODO: this was set to angle < -0.78... I changd it to angle_rad
     {
       // reset the accumulated errors here
       //start_flag = 0;   /// wait for restart
       //prev_time = 0;
       //motors.setSpeeds(0, 0);
-    } else if(start_flag) {
-      motors.setSpeeds((int)PWM_left, (int)PWM_right);
+    } else if (start_flag) {
+      //      motors.setSpeeds((int)PWM_left, (int)PWM_right);
     }
+    motors.setSpeeds((int)PWM_left, (int)PWM_right);
   }
 
   // kill switch
   if (buttonA.getSingleDebouncedPress())
   {
-      motors.setSpeeds(0,0);
-      armed_flag = 0;
-      start_flag = 0;
-      start_counter = 0;
-      setup();
-      while(!buttonA.getSingleDebouncedPress());
+    motors.setSpeeds(0, 0);
+    armed_flag = 0;
+    start_flag = 0;
+    start_counter = 0;
+    setup();
+    while (!buttonA.getSingleDebouncedPress());
   }
 }
